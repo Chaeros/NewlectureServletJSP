c2b2c52c2c968e84a460cae22d92893da301c95b
Chaeros <chpark6851@koreatech.ac.kr>
2023-06-18 11:11:49 +0900

    뉴렉처 서블릿/jsp 32강 실습 코드

--- JSPPj/src/com/newlecture/web/Calc2.java
+++ JSPPj/src/com/newlecture/web/Calc2.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,6 +22,9 @@ public class Calc2 extends HttpServlet{
 		
 		//세션을 이용하여 상태유지
 		HttpSession session = request.getSession();
+		
+		//쿠키들을 배열로 읽어옴
+		Cookie[] cookies = request.getCookies();
 		
 		//UTF-8 형식으로 전송하는 것
 		response.setCharacterEncoding("UTF-8");
@@ -40,10 +44,27 @@ public class Calc2 extends HttpServlet{
 		//값 계산
 		if(op.equals("=")) {
 			//int x = (Integer)application.getAttribute("value"); //앞선 서블릿에서의 값
-			int x = (Integer)session.getAttribute("value"); //앞선 서블릿에서의 값
+			//int x = (Integer)session.getAttribute("value"); //앞선 서블릿에서의 값
+			int x=0;
+			
+			for(Cookie c : cookies) {
+				if(c.getName().equals("value")) {
+					x=Integer.parseInt(c.getValue());
+					break;
+				}
+			}
+			
 			int y = v;  // 현재 서블릿에서의 값
 			//String operator = (String)application.getAttribute("op"); // 앞서 입력한 연산자
-			String operator = (String)session.getAttribute("op"); // 앞서 입력한 연산자
+			//String operator = (String)session.getAttribute("op"); // 앞서 입력한 연산자
+			String operator="";
+			
+			for(Cookie c : cookies) {
+				if(c.getName().equals("op")) {
+					operator=c.getValue();
+					break;
+				}
+			}
 			
 			int result = 0;
 			if(operator.equals("+"))
@@ -57,8 +78,21 @@ public class Calc2 extends HttpServlet{
 		{
 			//application.setAttribute("value", v);
 			//application.setAttribute("op", op);
-			session.setAttribute("value", v);
-			session.setAttribute("op", op);
+			//session.setAttribute("value", v);
+			//session.setAttribute("op", op);
+			
+			//쿠키를 생성하고 클라이언트에게 전송해준다.
+			//쿠키안에는 문자열만 저장시킬 수 있다.
+			Cookie valueCookie = new Cookie("value", String.valueOf(v));
+			Cookie opCookie = new Cookie("op", op);
+			
+			valueCookie.setPath("/calc2");
+			//setMaxAge는 초단위로 아래는 24시간을 의미한다.
+			valueCookie.setMaxAge(24*60*60);
+			opCookie.setPath("/calc2");
+			
+			response.addCookie(valueCookie);
+			response.addCookie(opCookie);
 		}
 	}
 }
